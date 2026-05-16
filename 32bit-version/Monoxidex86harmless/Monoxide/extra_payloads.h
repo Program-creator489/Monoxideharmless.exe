888fe777d701aa4cf2d2ccb5c38ab73a9d182057
Program-creator489 <kobralu21@gmail.com>
2026-05-16 18:41:06 -0500

    Add files via upload

---
+++ 32bit-version/Monoxidex86harmless/Monoxide/extra_payloads.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "monoxide.h"
+
+VOID
+GetRandomPath(
+    _Inout_ PWSTR szRandom,
+    _In_ INT nLength
+);
+
+VOID
+WINAPI
+MessageBoxThread( VOID );
+
+VOID
+WINAPI
+EnumGlobalWnd( VOID );
+
+VOID
+WINAPI
+CursorMess( VOID );
+
+VOID
+WINAPI
+CursorDraw( VOID );
